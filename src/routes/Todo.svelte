b11048634d1b72f9e872f6aabd3c115f1c7bf4a9
blakedreams <blake_kawtiash@live.ca>
2025-01-13 12:57:13 -0500

    1stDemoworkthru

---
+++ src/routes/Todo.svelte
@@ -0,0 +1,19 @@
+<script>
+let todoItem = $state('');
+let todoList = $state([]);
+
+function addItem() {
+     todoList.push(todoItem);
+}
+
+$inspect(todoList);
+
+</script>
+<form onsubmit={addItem}>
+<input type="text" bind:value={todoItem}>
+<button type="button">Add</button>
+</form>
+
+<style>
+
+</style>
